ba2e5f7529813c69ed0e53ca878c8f4178c7cb0b
Gianluca <gianluca@scarpellini.dev>
2020-09-25 07:24:55 +0200

    Event library

--- setup.py
+++ setup.py
@@ -5,13 +5,22 @@ setuptools.setup(
     version="0.1",
     author="gianscarpe",
     author_email="gianluca@scarpellini.dev",
-    description="Event-camera library",
+    description="Event library",
     url="https://github.com/gianscarpe/event_library",
+    entry_points = {
+        'console_scripts': ['recycle_for_event_camera = event_library.generator:main']
+    },
     packages=setuptools.find_packages(),
+    install_requires=[
+        'opencv-python',
+        'torch',
+        'torchvision',
+        'hydra-core'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GPL License",
-        "Operating System :: OS Independent",
+        "Operating System :: Linux",
     ],
     python_requires='>=3.6',
 )
